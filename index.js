6e5fcfb6db130ae53fb392e71705f9551cf0f86c
Ray Blasko <mray6288@gmail.com>
2017-11-24 02:51:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+  console.log('Hello')
   if string === string.toLowerCase(){
     return "I can't hear you!"
   }else if string === string.toUpperCase(){
